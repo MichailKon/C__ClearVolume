365e7c5ee3e39c4ddc65c2578e04e7fce0d0f2c2
MichailKon <misha2kon@mail.ru>
2021-05-18 06:45:06 +0300

    Added .gitignore

---
+++ cmake-build-debug/CMakeFiles/C__ClearVolume_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__ClearVolume_autogen"
+  "C__ClearVolume_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/C__ClearVolume_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
